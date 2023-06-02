1a08c5f9e8608906257ccdfd01e4af24d8d7e2aa
WeixuanKong <wkong@algomau.ca>
2023-06-02 05:42:44 -0400

    Update handle.py

--- project/handle.py
+++ project/handle.py
@@ -41,7 +41,7 @@ class Handle(object):
 				##add gpt related functions here
 				chatbot = OpenAIChatbot()
 				print("received: ", receivedMessage.Content)
-				content = chatbot.generate_response(receivedMessage.Content.toString())
+				content = chatbot.generate_response(receivedMessage.Content.decode("utf-8"))
 				replyMessage = reply.TextMessage(toUser, fromUser, content)
 				return replyMessage.send()
 
