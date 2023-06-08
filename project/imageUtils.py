3e945d0e043835f3616656295389f8ba87438383
WeixuanKong <wkong@algomau.ca>
2023-06-08 05:46:47 -0400

    img

--- project/imageUtils.py
+++ project/imageUtils.py
@@ -3,6 +3,9 @@ import base64
 import requests
 import accessToken
 
+## for testing
+import chatBot
+
 #not tested
 def b64_json_to_image(b64_json):
     # Decode the Base64 string into a UTF-8 encoded JSON string
@@ -20,7 +23,7 @@ def b64_json_to_image(b64_json):
 
 def url_to_image(url:str, file_name:str="image.jpg"):
     response = requests.get(url)
-    with open("project\{file_name}", "wb") as f:
+    with open(f"project\{file_name}", "wb") as f:
         f.write(response.content)
 
 import requests
@@ -42,5 +45,4 @@ def upload_image(access_token: str, filepath: str) -> str:
         return media_id
 
 if __name__ == "__main__":
-    res = upload_image("69_s29otVCXu1Q97yB4hIxZV86AT3VkcFNWZz7NEYo3OyQQMj86pNmNfq2wjo4rF_OwRZUd9JvghkOnoMG9jG8dVMtFf53I0GxRUpcwFN6u6YsV_2cwyzcdMOCQ-9wBAMhAIAUTF", "project\image.png")
-    print(res)
+    url_to_image(chatBot.create_image("A running cat"))
