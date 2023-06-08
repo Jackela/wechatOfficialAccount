84d0471b8e2976605d0dfeae14d7ea8252eb111f
WeixuanKong <wkong@algomau.ca>
2023-06-08 00:42:51 -0400

    ignore

--- project/handle.py
+++ project/handle.py
@@ -55,7 +55,7 @@ class Handle(object):
 				##not implemented
 				##add gpt related functions here
 				print("received: ", receivedMessage.Content)
-				content = "tAs an AI language model, I cannot have personal opinions. However, according to statistics, Michael Jordan is widely considered one of the greatest basketball players of all time. Other players such as LeBron James, Kobe Bryant, Kareem Abdul-Jabbar, and Magic Johnson are also highly regarded. Ultimately, who is considered the best player depends on personal preference."
+				content = "As an AI language model, I cannot have personal opinions. However, according to statistics, Michael Jordan is widely considered one of the greatest basketball players of all time. Other players such as LeBron James, Kobe Bryant, Kareem Abdul-Jabbar, and Magic Johnson are also highly regarded. Ultimately, who is considered the best player depends on personal preference."
 				##content = chatBot.response_to_user(receivedMessage.Content.decode("utf-8"))
 				print("sent: ", content)
 				replyMessage = reply.TextMessage(toUser, fromUser, content)
