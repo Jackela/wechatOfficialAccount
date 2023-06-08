cd0ef61c2816e5e579506ed4630c476b1dee65c6
WeixuanKong <wkong@algomau.ca>
2023-06-08 00:48:35 -0400

    Update handle.py

--- project/handle.py
+++ project/handle.py
@@ -57,8 +57,8 @@ class Handle(object):
 				print("received: ", receivedMessage.Content)
 				##content = "As an AI language model, I cannot have personal opinions. However, according to statistics, Michael Jordan is widely considered one of the greatest basketball players of all time. Other players such as LeBron James, Kobe Bryant, Kareem Abdul-Jabbar, and Magic Johnson are also highly regarded. Ultimately, who is considered the best player depends on personal preference."
 				content = chatBot.response_to_user(receivedMessage.Content.decode("utf-8"))
-				print("sent: ", content)
 				replyMessage = reply.TextMessage(toUser, fromUser, content)
+				print("sent: ", content)
 				return replyMessage.send()
 
 			if receivedMessage.MsgType == "image":
